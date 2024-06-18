d86e02adb98872ecad950064c6c41057502b5920
ndifix <yoshiki.linuxmint@gmail.com>
2024-06-18 15:14:48 +0900

    lib.rs を追加

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{env, fs, io::Read};
+use std::env;
+
+use pdf::get_pdf;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args = parse_args(&args);
 
-    read_file(&args.pdf_path);
+    get_pdf(&args.pdf_path);
 }
 
 struct Arguments {
@@ -22,17 +24,4 @@ fn parse_args(args: &Vec<String>) -> Arguments {
         self_path: args[0].clone(),
         pdf_path: args[1].clone(),
     }
-}
-
-fn read_file(path: &str) {
-    let mut file = match fs::File::open(path) {
-        Ok(file) => file,
-        Err(e) => panic!("failed to open file: {e}"),
-    };
-
-    let mut buf: Vec<u8> = Vec::new();
-    let _size = match file.read_to_end(&mut buf) {
-        Ok(size) => size,
-        Err(e) => panic!("failed to read file: {e}"),
-    };
 }
